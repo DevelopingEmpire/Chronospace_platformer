e8f1b1f8a6f18127969fdda9aae56278c5b6e75b
zox2m <zox2mm@gmail.com>
2024-01-17 06:09:11 +0900

    windkey~ 플레이어에 추가

--- Assets/scripts/WindKeyItem.cs
+++ Assets/scripts/WindKeyItem.cs
@@ -5,13 +5,14 @@ using DG.Tweening; // dot ween 사용
 
 public class WindKeyItem : MonoBehaviour
 {
-
+    public GameObject windKeyMesh; //자식인 mesh. 회전 시킬 대상 
     // Start is called before the first frame update
     void Start()
     {
         DOTween.Init(false, true, LogBehaviour.Verbose).SetCapacity(200, 50); // 먼지 몰겠는데 초기화라고 함. 기본 셋팅이니 안써도 됨 
 
-        transform.DORotate(new Vector3(90, 90, -90), 2f).SetLoops(3,LoopType.Incremental); // 회전각, 지속시간 .
+        windKeyMesh.transform.DOLocalRotate(new Vector3(0, 90, 0), 2f).SetLoops(3,LoopType.Incremental);// 회전각, 지속시간 .
+        
 
     }
 
