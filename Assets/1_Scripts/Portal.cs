3b0712e8bbcc8993791803099c4b67ef792fc10f
zox2m <zox2mm@gmail.com>
2024-05-20 16:40:18 +0900

    포탈 cs 합침

--- Assets/1_Scripts/Portal.cs
+++ Assets/1_Scripts/Portal.cs
@@ -55,6 +55,31 @@ public class Portal : StageMechanicsController
             StageManager.Instance.SetStageCleared(); // 스테이지 클리어 됨 
             SceneManager.LoadScene("Stage0");
         }
+
+        /*
+         
+         Debug.Log("포탈에 닿음");
+        // 플레이어가 액티브 포탈에 닿았는지 확인 
+        if (!(other.CompareTag("Player") && isActivated)) {
+            Debug.Log("활성화 안됨");
+            return;
+        }
+
+
+        Debug.Log("활성화 확인");
+        // 스테이지 입장 가능한 상태인지 확인 
+        if (!StageManager.Instance.CanEnterStage(targetScene)) return;
+        Debug.Log("스테이지 입장가능 ");
+
+        // 스테이지 이동 가즈아 
+        // StageManager.Instance.SetStageCleared(); // 현재 스테이지 클리어 됨 
+        StartCoroutine(GameManager.Instance.LoadScene(targetScene)); // 다음 스테이지 로드 - 비동기 이넘이므로 스타트 코루틴 
+        Debug.Log("게임매니저 - 로드씬 호출 됨 ");
+
+        // SceneManager.LoadScene(targetScene);  // 씬 로드하기 임시 
+         
+         
+         */
     }
 
     public override void Trigger()
